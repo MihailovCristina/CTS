cda50b6943aa4b6775a13db9e13933568dd3c100
MihailovCristina <cristinamihailov145@gmail.com>
2022-03-11 18:33:02 +0200

    Third Commit

---
+++ src/ro/ase/cts/contract/ExpressionNOSQL.java
@@ -0,0 +1,9 @@
+package ro.ase.cts.contract;
+
+import com.mongodb.client.MongoCollection;
+import org.bson.Document;
+
+public interface ExpressionNOSQL {
+    void expression(MongoCollection<Document> collection);
+
+}
---
+++ src/ro/ase/cts/contract/ExpressionSQL.java
@@ -0,0 +1,8 @@
+package ro.ase.cts.contract;
+
+import java.sql.Connection;
+
+public interface ExpressionSQL {
+    void expression(Connection connection);
+
+}
